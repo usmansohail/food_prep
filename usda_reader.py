326dc603af8a2573c0bff99c32d7935d04872a1a
usmansohail <msohail@usc.edu>
2019-07-14 01:17:02 -0700

    get_nutrients functionality works

--- usda_reader.py
+++ usda_reader.py
@@ -6,7 +6,10 @@ API_KEY = 'PRlaNFB3LpWOn14ICWIqupBkZa4ZKKdSkvpDwIJC'
 USDA_ENDPOINT = '@api.nal.usda.gov/fdc/v1/search'
 req_str2 = 'https://api.nal.usda.gov/ndb/search/?'              # < -------- the one that works
 API2 = 'Xcso2Fs6BTwI4pe5xIUXtM00ygqHq2UDq4hb4Qwv'               # < ------- -also the one that works
+calorie_endpoint = 'https://api.nal.usda.gov/ndb/reports/'
 
+
+# function that returns the response from a REST API get call 
 def request_get(api_key, endpoint, data):
     # concatinate the request
     #req_st = 'https://api_key=' + str(api_key) + str(endpoint)
@@ -29,7 +32,8 @@ def search_for_food(search_term, api_key=API2, sort='r'):
     return request_get(api_key, req_str2, search_dict).json()
 
 
-def get_calories_for_food(food_id, endpoint, api_key):
+# returns the number of calories given a food id 
+def get_nutrients_for_food(food_id, endpoint=calorie_endpoint, api_key=API2):
 
     # make the search dictionary 
     req_dict = {
@@ -37,7 +41,10 @@ def get_calories_for_food(food_id, endpoint, api_key):
         "ndbno":food_id,
         "format":"json"
     }
-    return request_get(api_key, endpoint, req_dict).json()
+    response  =  request_get(api_key, endpoint, req_dict).json()
+
+    # get the nutrients for the item
+    return response['report']['food']['nutrients']
 
 # try a few searches
 js_res = search_for_food("raw potato")
@@ -50,13 +57,16 @@ for item in js_res['list']['item']:
     item_ndbnos[item['name']] = item['ndbno']
 
 
+"""
 # try and print detailed nutrition info for some item
 for item in item_ndbnos.keys():
     # try and get the info for the ndbno id
-    response = get_calories_for_food(item_ndbnos[item], req_str2, API2)
+    nutrients = get_nutrients_for_food(item_ndbnos[item], calorie_endpoint, API2)
     print('For item: {} \n                 '.format(item))
-    for key in response['list']['item']:
+    for key in nutrients:
         print("--             ", key)
+"""
+
 ENDPOINT2 = 'https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=Xcso2Fs6BTwI4pe5xIUXtM00ygqHq2UDq4hb4Qwv&location=Denver+CO'
 
 
@@ -65,5 +75,7 @@ req_2 = ENDPOINT2
 #print(requests.get(req_2).json())
 
 #print(requests.get(req_str2, test_dict).json())
+key = list(item_ndbnos.keys())[0]
+print('nutrients: ', get_nutrients_for_food(item_ndbnos[key]))
 
-
+#get_nutrients_for_food(key)
